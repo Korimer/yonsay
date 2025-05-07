f1dee11f5360f33211bf0b26c25a8d1f8a9fb289
Korimer <thekclov@gmail.com>
2025-05-07 00:38:12 -0600

    indent compatibility

--- Yon-Say.ps1
+++ Yon-Say.ps1
@@ -7,19 +7,23 @@ $textx = "130"
 $texty = "20"
 
 $maxline = 70 # how many characters per line
+$firstline = "C:  "
+$outfile = ".tmp"
 
 $background = Join-Path -Path $resources -ChildPath $bggif -Resolve
+$indent = " " * $firstline.Length
+$escapedfirst = ($firstline -replace '(?<Escape>[,:"\\])','\${Escape}')-replace '\\', '\\'
 
-
-$basetext = Read-Host -Prompt "Yon says: "
+$basetext = Read-Host -Prompt "Yon says"
 
 $chrcnt = 0
-$fnltxt = ""
+$fnltxt = $escapedfirst
 foreach ($line in ($basetext -split "( )")) {
-    $escapedtext = $line -replace '(?<Escape>[,:"\\])','\${Escape}'
+    $escapedtext = ($line -replace '(?<Escape>[,:"\\])','\${Escape}') -replace '\\\\', ("\"*8)
+    echo $escapedtext
     $chrcnt += $line.Length 
     if ($chrcnt -gt $maxline) {
-        $fnltxt += "`n"
+        $fnltxt += "`n" + $indent
         $chrcnt = 0
     }
     $fnltxt += $escapedtext
