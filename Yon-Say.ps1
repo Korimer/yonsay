0cf1098396d05c8623b9857aed0f89e51d0a4281
Korimer <thekclov@gmail.com>
2025-05-07 22:26:53 -0600

    figured out dithering, progress on forcing the correct indent

--- Yon-Say.ps1
+++ Yon-Say.ps1
@@ -6,41 +6,55 @@ $output = "output.gif"
 $textx = "130"
 $texty = "20"
 
-$maxline = 70 # how many characters per line
+$maxline = 69 # how many characters per line
 $firstline = "C:  "
-$outfile = ".tmp"
+$tempfile = ".tmp"
 
 $background = Join-Path -Path $resources -ChildPath $bggif -Resolve
 $indent = " " * $firstline.Length
 
 $basetext = Read-Host -Prompt "Yon says"
 
+# WIP
+# $splittext = (($basetext -split "( )") -replace "\\n","`n") -split ""
+
+# $replacementmap = [ordered]@{
+#     "\\" = "\\"
+#     " +" = '$1'
+#     "(\\n|\n)" = "`n"+$indent
+# }
+
 $chrcnt = 0
-Out-File -Filepath $outfile -InputObject $firstline -NoNewline -Encoding utf8
+Out-File -Filepath $tempfile -InputObject $firstline -NoNewline -Encoding utf8
 foreach ($line in ($basetext -split "( )")) {
-    $escapedtext = $line -replace '\\','\\'
+    $escapedtext = ($line -replace '\\','\\') -replace "(\\n|`n)", ("`n"+$indent)
     $chrcnt += $line.Length 
     if ($chrcnt -gt $maxline) {
-        Out-File -FilePath $outfile -Append -InputObject ("`n" + $indent) -NoNewline -Encoding utf8
+        Out-File -FilePath $tempfile -Append -InputObject ("`n" + $indent) -NoNewline -Encoding utf8
         $chrcnt = 0
     }
-    Out-File -FilePath $outfile -Append -InputObject $escapedtext -NoNewline -Encoding utf8
+    Out-File -FilePath $tempfile -Append -InputObject $escapedtext -NoNewline -Encoding utf8
 }
 
-$ffmpegcommand = (
+$textcommand = (
     "drawtext=fontfile=""Kiwi Fruit.otf""",
     "fontsize=$fontsize",
     "fontcolor=$fontcolor",
     "x=$textx",
     "y=$texty",
-    """textfile=$outfile"""
+    """textfile=$tempfile"""
 ) -join (":")
+$filtercommand = "split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse"
 
-[Array]$imgargs =
+$completefilter = ($textcommand,$filtercommand) -join(",")
+
+[Array]$completeargs =
     "-i",
     "$background",
     "-vf",
-    $ffmpegcommand,
+    "$completefilter",
     "$output"
 
-& ffmpeg.exe $imgargs
+& ffmpeg.exe $completeargs 
+
+Remove-Item $tempfile
