d8235e1b670c401323f8433adf071f5d1710a6e6
Korimer <thekclov@gmail.com>
2025-05-09 13:26:41 -0600

    basic navigation

--- SelectSprites.ps1
+++ SelectSprites.ps1
@@ -1,8 +1,32 @@
 $resources = Resolve-Path "SpriteResources"
 
-$active = $null
+function List-Options {
+
+    param (
+        [Array]$arr
+    )
+
+    Write-Output "0 - Return"
+    for ($i=1; $i -le $arr.Length; $i++) {
+        Write-Output "$i - $($arr[$i-1])"
+    }
+}
+
+$curfolder = $resources
 while ($true) {
-    $active = Get-ChildItem $resources -Directory
-    $active
-    break
+    $active = Get-ChildItem $curfolder -Directory
+    List-Options $active
+    $usrinput = Read-Host "Choose"
+    if ($usrinput -eq 0) {
+        if ($curfolder -eq $resources) {break}
+        else {$curfolder = (Split-Path $curfolder -Parent -Resolve); echo $curfolder}
+    }
+    else {
+        if ($usrinput -le $active.Length) {
+            $curfolder = Join-Path -Path $curfolder -ChildPath $active[$usrinput-1]
+        }
+        else {
+            Write-Output "Error: invalid choice. try again"
+        }
+    }
 }
